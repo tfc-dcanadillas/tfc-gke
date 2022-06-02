cda1c7e16c4abc2bd875c2fb1e2110c8bcd9a70e
dcanadillas <dcanadillas@hashicorp.com>
2022-06-02 12:05:44 +0200

    test of pr

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ data "google_container_engine_versions" "k8sversion" {
 }
 
 resource "google_container_cluster" "primary" {
+	# checkov:skip=BC_GCP_KUBERNETES_26: ADD REASON
   # provider = google-beta
   # project = var.gcp_project
   name     = var.gke_cluster
